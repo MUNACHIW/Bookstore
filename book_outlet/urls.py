74cb98ccb18003b3ae78150e15c0196fb4b8ab79
MUNACHIW <munasom25@gmail.com>
2024-08-20 01:34:57 +0100

    added a slug and some new features to the software

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.index), path("<int:id>", views.book_detail)]
+urlpatterns = [
+    path("", views.index),
+    path("<slug:slug>", views.book_detail, name="book-detail"),
+]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -4,24 +4,36 @@ from django.http import Http404
 # Create your views here.
 from .models import Book
 from django import template
+from django.db.models import Avg, Max, Min
 
 
 def index(request):
-    Books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {"books": Books})
+    Books = Book.objects.all().order_by(" rating")
+    numbooks = Books.count()
+    avg_rating = Books.aggregate(Avg("rating"))
+    return render(
+        request,
+        "book_outlet/index.html",
+        {
+            "books": Books,
+            "total_number_of_books": numbooks,
+            "average_rating": avg_rating,
+        },
+    )
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
 
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(
         request,
         "book_outlet/book_detail.html",
         {
+            "id": book.id,
             "title": book.title,
             "author": book.author,
             "rating": book.rating,
