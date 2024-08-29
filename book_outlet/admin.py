17fa0b548465e89b06700f000154d3564d38736e
MUNACHIW <munasom25@gmail.com>
2024-08-29 01:25:18 +0100

    final software modifications

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Author
+from .models import Book, Author, Address, Country
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -12,5 +12,7 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
 
 # Register your models here.
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,9 +8,36 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}{self.code}"
+
+    class Meta:
+        verbose_name_plural = "Countries entries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def detail(self):
+        return f"{self.street}  {self.postal_code}  {self.city}"
+
+    def __str__(self):
+        return self.detail()
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     # def __str__(self):
     #     return f"{self.first_name}{self.last_name}"
@@ -32,6 +59,8 @@ class Book(models.Model):
     )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, blank=True, db_index=True)
+    publish_countries = models.ManyToManyField(Country)
+    # for many to many ondelete function is not needed
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
