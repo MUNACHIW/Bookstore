d51dc62cefee7b3c964b8a54900ead3176ba0259
MUNACHIW <munasom25@gmail.com>
2024-09-23 11:48:18 +0100

    working with imagefield and validation

--- profiles/models.py
+++ profiles/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
+
+
+## while working with imagefield you need to install an extra package python -m pip install
+## Pillow
